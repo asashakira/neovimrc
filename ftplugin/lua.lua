eb6200fc9539e8c662dd239caecc59deaad0184e
akira <asa.shakira340@gmail.com>
2025-03-24 12:16:16 +0900

    add lua specific options

---
+++ ftplugin/lua.lua
@@ -0,0 +1,6 @@
+local tab = 2
+
+vim.opt_local.expandtab = true
+vim.opt_local.softtabstop = tab
+vim.opt_local.shiftwidth = tab
+vim.opt_local.formatoptions:append({ c = true, r = true, o = true, q = true })
