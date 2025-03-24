d784ee943c6ed1ee2084fccc1cf5b434b97274b0
akira <asa.shakira340@gmail.com>
2025-03-24 12:15:44 +0900

    use ruff to format python

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "ruff" } },
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["python"] = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+      },
+    },
+  },
+}
